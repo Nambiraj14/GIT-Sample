f316688b14d6aa326b4144ccc7e83344904e321c
narayanan1381998 <narayanan1381998@gmail.com>
2022-12-07 15:14:54 +0530

    Narayanan Code

--- src/test/java/com/maven/Sample1.java
+++ src/test/java/com/maven/Sample1.java
@@ -13,5 +13,15 @@ public class Sample1 {
 		System.out.println(3);
 
 	}
+	private void narayanan1() {
+		System.out.println(hello);
+	}
+	private void narayanan2() {
+		System.out.println(hello);
+	}
+
+	private void narayanan3() {
+		System.out.println(hello);
+	}
 
 }
